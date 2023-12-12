b94062249e2b3429d473e464d975287858a4603c
Clément Lherbier <clement.lherbier@reewayy.io>
2023-12-12 17:31:07 +0100

    first commit

---
+++ cdsicount.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+from function import scraping_bdm2
+
+st.set_page_config(
+    page_title="Streamlit App",
+    page_icon="🧊",
+    layout="wide",
+)
+
+# Champs de saisie de text pour les mots clef avec un bouton de validation
+# scrapper en fonction de la recherche les artciles surl e site BDM
+# Permettre à l'applidation de téélcharger les données en csv
+
+st.title('Super Scrappeur BDM')
+
+search = st.text_input('Recherche', placeholder='Adobe, web, IA, ...')
+pages = st.slider('Nombre de pages', 1, 10, 1)
+
+if st.button('Valider'):
+    st.write('Vous avez choisi', search)
+
+    with st.spinner('Chargement en cours...'):
+
+        results_dict = {}
+
+        for index_page in range(1, pages+1): 
+            url = f'https://www.blogdumoderateur.com/page/{str(index_page)}/'
+
+            search_clean = search.replace(' ', '+')
+            url_search = url + '?s=' + search_clean
+
+            try : 
+                dict_page = scraping_bdm2(url_search)
+                results_dict.update(dict_page)
+            except:
+                break
+
+        df = pd.DataFrame(results_dict).T
+        st.write(df)
+
+        file_name = search.replace(' ', '_') + '.csv'
+
+        st.download_button(
+            label="Télécharger les données",
+            data=df.to_csv().encode('utf-8'),
+            file_name=file_name,
+            mime='text/csv'
+        )
+
+    st.snow()
+    st.toast('Scraping terminé !', icon='🎉')
---
+++ function.py
@@ -0,0 +1,43 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def scraping_bdm2(url):
+    print (url)
+    driver = webdriver.Chrome('')
+    driver.get(url)
+
+    article_dict = {}
+    articles = driver.find_elements(By.CLASS_NAME, 'pdt-item')
+
+    for article in articles:
+        id = article.get_attribute('id')
+
+        title = article.find_element(By.CLASS_NAME, 'title-3').text
+
+        try :
+            image = article.find_element(By.TAG_NAME, 'img').get_attribute('src')
+        except:
+            image = None
+
+        try:
+            link = article.find_element(By.TAG_NAME, 'a' ).get_attribute('href')           
+        except:
+            link = None
+
+        price = article.find_element(By.CLASS_NAME, 'price').text
+
+        #stars = article.find_element(By.XPATH, '//html/body/div[4]/div/div[3]/div/div/div[2]/div[2]/ul[1]/li[14]/div[2]/div[1]/div[2]/a/span').tex
+
+        elements = driver.find_elements(By.CSS_SELECTOR,'[class^="star-"]')
+
+# Parcourez chaque élément et récupérez la partie après "star-"
+        for element in elements:
+            classe = element.get_attribute("class")
+            partie_apres_star = classe.split("star-")[1]
+       
+
+        article_dict[id] = {'title': title, 'notation': partie_apres_star,
+                            'link': link, 'image': image, 'prix': price}
+    return article_dict
